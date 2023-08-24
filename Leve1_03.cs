f29888a2717338056271419086684a1ddf1830ba
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-24 10:14:10 +0900

    자릿수 더하기

---
+++ Leve1_03.cs
@@ -0,0 +1,18 @@
+﻿public class Leve1_03
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            while (n % 10 > 0 || n >= 10)
+            {
+                answer += n % 10;
+                n = n / 10;
+            }
+
+            return answer;
+        }
+    }
+}
