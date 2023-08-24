d76b82267c4298d9691a056e6c5b4c2123da86b6
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-24 10:01:22 +0900

    최초 등록

---
+++ Leve1_01.cs
@@ -0,0 +1,11 @@
+﻿public class Leve1_01
+{
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            string answer = num % 2 == 0 ? "Even" : "Odd";
+            return answer;
+        }
+    }
+}
