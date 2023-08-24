a767e527f21fd34ab023ca73e15a46cbd307d3fa
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-24 10:33:11 +0900

    나머지가 1이 되는 수 찾기

---
+++ Leve1_05.cs
@@ -0,0 +1,17 @@
+﻿public class Leve1_05
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = n;
+            int i = n;
+            while (i > 1)
+            {
+                if (n % i == 1) answer = i;
+                --i;
+            }
+            return answer;
+        }
+    }
+}
