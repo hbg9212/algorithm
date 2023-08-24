b6bb065251e42bd9acfde4a6d1599136ec574b23
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-24 10:29:06 +0900

    약수의 합

---
+++ Leve1_04.cs
@@ -0,0 +1,20 @@
+﻿public class Leve1_04
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 1; i < n / 2; i++)
+            {
+                if (n % i == 0) answer += i;
+            }
+            answer += n % 2 == 0 ? n / 2 : 0;
+
+            answer += n;
+
+            return answer;
+        }
+    }
+}
