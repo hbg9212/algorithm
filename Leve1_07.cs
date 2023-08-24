374049798c5e6c629d94f565c71560ca7ce92878
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-24 11:06:57 +0900

    자연수 뒤집어 배열로 만들기

---
+++ Leve1_07.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Leve1_07
+{
+
+    public class Solution
+    {
+        public int[] solution(long n)
+        {
+            int c = (int)Math.Log10(n);
+            int[] answer = new int[++c];
+            for (int i = 0; i < c; i++)
+            {
+                answer[i] = (int)(n % 10);
+                n = n / 10;
+            }
+
+            return answer;
+        }
+    }
+}
