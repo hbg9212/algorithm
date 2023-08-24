6eb3a27bd70f9b7ea30ee471f11cc5fd97a8d0bf
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-24 10:39:54 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Leve1_06.cs
@@ -0,0 +1,17 @@
+﻿public class Leve1_06
+{
+    public class Solution
+    {
+        public long[] solution(int x, int n)
+        {
+            long[] answer = new long[n];
+
+            for (long i = 0; i < n; i++)
+            {
+                answer[i] = x + (x * i);
+            }
+
+            return answer;
+        }
+    }
+}
